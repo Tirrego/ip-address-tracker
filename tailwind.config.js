d34f876db6e75936aa567a2525c9d794f8290e78
Tirrego <g.schmittel@gmx.de>
2024-02-29 13:33:31 +0100

    Font fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,13 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        VeryDarkGray: "hsl(0, 0%, 17%)",
+        DarkGray: "hsl(0, 0%, 59%)",
       },
+      fontFamily: {
+        rubik: ['var(--font-rubik)']
+      }
     },
   },
   plugins: [],
